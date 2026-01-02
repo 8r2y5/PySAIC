4ca7959b18eaacd48c230b4080bc30ac7a6facf0
Krzysztof Kwasniak <mr.brzys@gmail.com>
2026-01-02 12:13:48 +0100

    Refactor config handling with new field types and color configuration

--- src/pysaic/config.py
+++ src/pysaic/config.py
@@ -1,7 +1,7 @@
 import logging
-from dataclasses import asdict, dataclass, field
+from dataclasses import asdict, dataclass, field, MISSING
 from enum import StrEnum
-from typing import Optional, Union
+from typing import Optional, Callable, Any, Self
 
 import yaml
 
@@ -21,6 +21,35 @@ from pysaic.crc_strings.use_case import random_name
 logger = logging.getLogger(__name__)
 
 DEFAULT_STATIC_AVATAR = f"crc_icon_{FactionsEnum.Loner.value}_1"
+
+
+def config_field(
+    *,
+    default: Any = MISSING,
+    default_factory: Any = MISSING,
+    init=True,
+    repr=True,
+    hash=None,
+    compare=True,
+    kw_only=MISSING,
+    loading: None | Callable = None,
+    dumping: None | Callable = None,
+    own_save: bool = False,
+):
+    return field(
+        default=default,
+        default_factory=default_factory,
+        init=init,
+        repr=repr,
+        hash=hash,
+        compare=compare,
+        metadata={
+            "loading": loading,
+            "dumping": dumping,
+            "own_save": own_save,
+        },
+        kw_only=kw_only,
+    )
 
 
 class InGameUserDisplayEnum(StrEnum):
@@ -102,12 +131,53 @@ class Server:
 
 
 @dataclass
+class ColorsConfig:
+    time: str = "floral white"
+    text: str = "ghost white"
+    highlight: str = "gray50"
+    hyper_link: str = "#3B8ED0"
+    information: str = "lightblue"
+    error: str = "red3"
+    clear_sky: str = "deep sky blue"
+    loner: str = "light goldenrod"
+    ecologist: str = "darkorange"
+    bandit: str = "sienna3"
+    monolith: str = "DarkOrchid3"
+    duty: str = "firebrick1"
+    freedom: str = "spring green"
+    mercenary: str = "dodgerblue"
+    renegade: str = "green yellow"
+    zombie: str = "black"
+    anonymous: str = "black"
+    unisg: str = "salmon"
+    sin: str = "maroon4"
+    direct_message: str = "hot pink"
+    online: str = "green"
+    offline: str = "red"
+    afk: str = "yellow"
+    background: str = "gray30"
+
+    @classmethod
+    def load_from_config(cls, config: None | dict[str, str] = None):
+        try:
+            return cls(
+                **{
+                    field: config.get(field) or value.default
+                    for field, value in cls.__dataclass_fields__.items()
+                }
+            )
+        except Exception:
+            logger.exception("Cannot load config for colors, creating default")
+            return cls()
+
+
+@dataclass
 class FontConfig:
     name: str = "Segoe UI"
     size: int = 11
 
     @classmethod
-    def load_from_config(cls, config):
+    def load_from_config(cls, config: None | dict[str, int | str] = None):
         try:
             return cls(
                 config.get("name") or cls.name, config.get("size") or cls.size
@@ -117,43 +187,125 @@ class FontConfig:
             return cls()
 
 
+def to_enum(enum_class, value, default):
+    try:
+        return enum_class[value]
+    except (KeyError, TypeError):
+        return default
+
+
+def _to_bool(param: str | bool, default: bool = True) -> bool:
+    if isinstance(param, bool):
+        return param
+    try:
+        return param.lower() == "true"
+    except AttributeError:
+        return default
+
+
+def config_bool_field(field_name, default):
+    return config_field(
+        default=default, loading=lambda x: _to_bool(x.get(field_name), default)
+    )
+
+
+def config_enum_field(field_name, default):
+    return config_field(
+        default=default,
+        loading=lambda x: to_enum(
+            default.__class__,
+            x.get(field_name),
+            default=default,
+        ),
+        dumping=lambda x: getattr(x, field_name).name,
+    )
+
+
+def load_and_sanitize_nick(config):
+    from pysaic.use_cases.nick import sanitize_nick
+
+    return sanitize_nick(config.get("nick") or random_name().replace(" ", "_"))
+
+
 @dataclass
 class Config:
-    nick: str
-    server: Server
-    password: str
-    faction_setting: FactionSetting = FactionSetting.GameSynced
-    current_faction: FactionsEnum = FactionsEnum.Loner
-    news_duration: int = 3250
+    nick: str = config_field(
+        default=lambda x: random_name().replace(" ", "_"),
+        loading=load_and_sanitize_nick,
+    )
+    password: str = ""
+    server: Server = config_field(
+        loading=lambda x: Server.load_config(),
+        dumping=lambda x: getattr(x, "server").save_config(),
+        own_save=True,
+        default_factory=Server,
+    )
+    faction_setting: FactionSetting = config_enum_field(
+        field_name="faction_setting", default=FactionSetting.GameSynced
+    )
+    current_faction: FactionsEnum = config_enum_field(
+        field_name="current_faction", default=FactionsEnum.Loner
+    )
+    news_duration: int = config_field(
+        default=3250, loading=lambda x: int(x.get("news_duration") or 3250)
+    )
     chat_key: str = "DIK_RETURN"
     nick_auto_complete_key: str = "DIK_TAB"
-    news_sound: bool = True
-    close_chat: bool = True
+    news_sound: bool = config_bool_field("news_sound", True)
+    close_chat: bool = config_bool_field("close_chat", True)
     disconnect_when_emission: DisconnectOnNetworkDestructionSetting = (
-        DisconnectOnNetworkDestructionSetting.MalformSignalOnly
+        config_enum_field(
+            field_name="disconnect_when_emission",
+            default=DisconnectOnNetworkDestructionSetting.MalformSignalOnly,
+        )
     )
     disconnect_when_underground: DisconnectOnNetworkDestructionSetting = (
-        DisconnectOnNetworkDestructionSetting.MalformSignalOnly
+        config_enum_field(
+            field_name="disconnect_when_underground",
+            default=DisconnectOnNetworkDestructionSetting.MalformSignalOnly,
+        )
     )
-    block_money_transfer: bool = True
+    block_money_transfer: bool = config_bool_field(
+        "block_money_transfer", True
+    )
     user_list_display: str = GroupByFactionWithCounter.name
-    accept_dms_from_not_in_the_channel: bool = False
+    accept_dms_from_not_in_the_channel: bool = config_bool_field(
+        "accept_dms_from_not_in_the_channel", False
+    )
     avatar: str = AvatarEnum.faction_and_name_based.value
     current_avatar: str = f"{FactionsEnum.Loner.value}_1"
     blocked_users: dict[str, set[str]] = field(default_factory=dict)
     blocked_words: list[str] = field(default_factory=list)
-    in_game_users_display: InGameUserDisplayEnum = (
-        InGameUserDisplayEnum.PySAIC_Card
+    in_game_users_display: InGameUserDisplayEnum = config_enum_field(
+        field_name="in_game_users_display",
+        default=InGameUserDisplayEnum.PySAIC_Card,
     )
     in_game_users_display_order: InGameUserDisplayOrderEnum = (
-        InGameUserDisplayOrderEnum.Faction
+        config_enum_field(
+            field_name="in_game_users_display_order",
+            default=InGameUserDisplayOrderEnum.Faction,
+        )
     )
-    faction_colored_nicks: bool = True
-    death_report_type: DeathReportTypeEnum = DeathReportTypeEnum.OnlineFactions
-    death_reports: bool = True
-    pop_up_on_ping: bool = False
-    pop_up_sound: bool = True
-    font: FontConfig = field(default_factory=FontConfig)
+    faction_colored_nicks: bool = config_bool_field(
+        "faction_colored_nicks", True
+    )
+    death_report_type: DeathReportTypeEnum = config_enum_field(
+        field_name="death_report_type",
+        default=DeathReportTypeEnum.OnlineFactions,
+    )
+    death_reports: bool = config_bool_field("death_reports", True)
+    pop_up_on_ping: bool = config_bool_field("pop_up_on_ping", False)
+    pop_up_sound: bool = config_bool_field("pop_up_sound", True)
+    font: FontConfig = config_field(
+        default_factory=FontConfig,
+        loading=lambda x: FontConfig.load_from_config(x.get("font")),
+        dumping=lambda x: asdict(x.font),
+    )
+    colors: ColorsConfig = config_field(
+        default_factory=ColorsConfig,
+        loading=lambda x: ColorsConfig.load_from_config(x.get("colors")),
+        dumping=lambda x: asdict(x.colors),
+    )
 
     @classmethod
     def load_config(cls):
@@ -178,164 +330,48 @@ class Config:
         return instance
 
     def save_config(self):
-        with open("config.yml", "w") as f:
-            yaml.dump(
-                {
-                    "nick": self.nick,
-                    "password": self.password,
-                    "faction_setting": self.faction_setting.value,
-                    "current_faction": self._parse_to_yaml_faction(
-                        self.current_faction or FactionsEnum.Loner
-                    ),
-                    "news_duration": self.news_duration,
-                    "chat_key": self.chat_key,
-                    "nick_auto_complete_key": self.nick_auto_complete_key,
-                    "news_sound": self.news_sound,
-                    "close_chat": self.close_chat,
-                    "disconnect_when_emission": self.disconnect_when_emission.name,
-                    "disconnect_when_underground": self.disconnect_when_underground.name,
-                    "block_money_transfer": self.block_money_transfer,
-                    "user_list_display": self.user_list_display,
-                    "avatar": self.avatar,
-                    "current_avatar": self.current_avatar,
-                    "accept_dms_from_not_in_the_channel": self.accept_dms_from_not_in_the_channel,
-                    "blocked_users": self.blocked_users,
-                    "blocked_words": self.blocked_words,
-                    "in_game_users_display": self.in_game_users_display.name,
-                    "in_game_users_display_order": self.in_game_users_display_order.name,
-                    "faction_colored_nicks": self.faction_colored_nicks,
-                    "death_report_type": self.death_report_type.name,
-                    "death_reports": self.death_reports,
-                    "pop_up_on_ping": self.pop_up_on_ping,
-                    "pop_up_sound": self.pop_up_sound,
-                    "font": asdict(self.font),
-                },
-                f,
-            )
-            self.server.save_config()
+        data = {}
+        separate_save = []
+        for field_name, field_value in self.__dataclass_fields__.items():
+            if (
+                field_value.metadata
+                and field_value.metadata.get("own_save") is True
+            ):
+                separate_save.append(field_value.metadata["dumping"])
+                continue
 
-    @staticmethod
-    def _parse_to_yaml_faction(faction: Union[FactionsEnum, str]) -> str:
-        try:
-            return faction.name
-        except AttributeError:
-            return faction
+            if dump_function := (
+                field_value.metadata and field_value.metadata.get("dumping")
+            ):
+                value = dump_function(self)
+            else:
+                value = getattr(self, field_name)
+
+            data[field_name] = value
+
+        with open("config.yml", "w") as f:
+            yaml.dump(data, f)
+            for callable in separate_save:
+                callable(self)
 
     @classmethod
     def _default_config(cls) -> dict:
-        return {
-            "nick": random_name().replace(" ", "_"),
-            "password": "",
-            "faction_setting": cls.faction_setting.value,
-            "current_faction": cls.current_faction.name,
-            "news_duration": cls.news_duration,
-            "chat_key": cls.chat_key,
-            "nick_auto_complete_key": cls.nick_auto_complete_key,
-            "news_sound": cls.news_sound,
-            "close_chat": cls.close_chat,
-            "disconnect_when_emission": cls.disconnect_when_emission,
-            "disconnect_when_underground": cls.disconnect_when_underground,
-            "block_money_transfer": cls.block_money_transfer,
-            "user_list_display": cls.user_list_display,
-            "avatar": cls.avatar,
-            "current_avatar": cls.current_avatar,
-            "accept_dms_from_not_in_the_channel": cls.accept_dms_from_not_in_the_channel,
-            "blocked_users": {},
-            "blocked_words": [],
-            "in_game_users_display": cls.in_game_users_display.name,
-            "in_game_users_display_order": cls.in_game_users_display_order.name,
-            "faction_colored_nicks": cls.faction_colored_nicks,
-            "death_report_type": cls.death_report_type.name,
-            "death_reports": cls.death_reports,
-            "pop_up_on_ping": cls.pop_up_on_ping,
-            "pop_up_sound": cls.pop_up_sound,
-            "font": cls.font,
-        }
+        return {}
 
     @classmethod
-    def _to_bool(cls, param: str | bool, default: bool = True) -> bool:
-        if isinstance(param, bool):
-            return param
-        try:
-            return param.lower() == "true"
-        except AttributeError:
-            return default
+    def _create_instance_from_config(cls, config: dict) -> Self:
+        data = {}
+        for field_name, field_value in cls.__dataclass_fields__.items():
+            if load_func := (
+                field_value.metadata and field_value.metadata.get("loading")
+            ):
+                value = load_func(config)
+            else:
+                value = config.get(field_name)
 
-    @classmethod
-    def _create_instance_from_config(cls, config: dict):
-        current_faction = FactionsEnum[
-            config.get("current_faction") or FactionsEnum.Loner.name
-        ]
-        from pysaic.use_cases.nick import sanitize_nick
+            data[field_name] = value
 
-        return cls(
-            nick=sanitize_nick(config["nick"]),
-            server=Server.load_config(),
-            password=config["password"],
-            faction_setting=FactionSetting(config["faction_setting"]),
-            current_faction=current_faction,
-            news_duration=int(config["news_duration"]),
-            chat_key=config["chat_key"],
-            nick_auto_complete_key=config["nick_auto_complete_key"],
-            news_sound=cls._to_bool(
-                config["news_sound"], default=cls.news_sound
-            ),
-            close_chat=cls._to_bool(
-                config["close_chat"], default=cls.close_chat
-            ),
-            disconnect_when_emission=cls._to_enum(
-                DisconnectOnNetworkDestructionSetting,
-                config.get("disconnect_when_emission"),
-                default=cls.disconnect_when_emission,
-            ),
-            disconnect_when_underground=cls._to_enum(
-                DisconnectOnNetworkDestructionSetting,
-                config.get("disconnect_when_underground"),
-                default=cls.disconnect_when_underground,
-            ),
-            block_money_transfer=cls._to_bool(
-                config["block_money_transfer"],
-                default=cls.block_money_transfer,
-            ),
-            user_list_display=config.get("user_list_display")
-            or cls.user_list_display,
-            accept_dms_from_not_in_the_channel=cls._to_bool(
-                config.get("accept_dms_from_not_in_the_channel"),
-                default=cls.accept_dms_from_not_in_the_channel,
-            ),
-            avatar=cls._parse_avatar(config.get("avatar") or cls.avatar),
-            current_avatar=cls._parse_static_avatar(
-                config.get("current_avatar") or cls.current_avatar,
-            ),
-            blocked_users=config.get("blocked_users") or {},
-            blocked_words=config.get("blocked_words") or {},
-            in_game_users_display=InGameUserDisplayEnum[
-                config.get("in_game_users_display")
-                or cls.in_game_users_display.value
-            ],
-            in_game_users_display_order=cls._to_enum(
-                InGameUserDisplayOrderEnum,
-                config.get("in_game_users_display_order"),
-                default=cls.in_game_users_display_order.name,
-            ),
-            faction_colored_nicks=cls._to_bool(
-                config.get("faction_colored_nicks"),
-                default=cls.faction_colored_nicks,
-            ),
-            death_report_type=DeathReportTypeEnum[
-                config.get("death_report_type") or cls.death_report_type.name
-            ],
-            death_reports=cls._to_bool(
-                config.get("death_reports"), default=cls.death_reports
-            ),
-            pop_up_on_ping=cls._to_bool(
-                config.get("pop_up_on_ping"), default=cls.pop_up_on_ping
-            ),
-            pop_up_sound=cls._to_bool(
-                config.get("pop_up_sound"), default=cls.pop_up_sound
-            ),
-            font=FontConfig.load_from_config(config),
-        )
+        return cls(**data)
 
     @classmethod
     def _parse_static_avatar(cls, value: Optional[str]) -> str:
@@ -416,14 +452,8 @@ class Config:
             self.current_avatar = f"crc_icon_{self.current_faction.value}_1"
             return True
 
-    @classmethod
-    def _to_enum(cls, enum_class, value, default):
-        try:
-            return enum_class[value]
-        except (KeyError, TypeError):
-            return default
-
 
 if __name__ == "__main__":
     config = Config.load_config()
     print(config)
+    config.save_config()
